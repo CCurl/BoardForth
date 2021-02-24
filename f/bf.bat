ba043153120bdf7d8dd1dbb38b904c5da5636658
Chris Curl <curlbud-github@yahoo.com>
2021-02-24 17:02:01 +0000

    Cleanup repo

--- f/bf.bat
+++
@@ -1,3 +0,0 @@
-cd ..\pc
-boardforth.exe
-cd ..\f
--- f/make.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-if exist f.exe ( del f.exe )
-gcc -o f.exe -fpermissive -Wwrite-strings f.cpp prims.cpp loader.cpp opcodes.cpp
-if xx%1xx == xxxx goto :done
-if exist f.exe ( f.exe )
-
-:done
