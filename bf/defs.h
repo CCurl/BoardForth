abf56d5fae399d3a2a2f102845bd1e2a7c976dfd
Chris Curl <curlbud-github@yahoo.com>
2021-04-14 17:10:49 -0400

    Update defs.h

--- bf/defs.h
+++ bf/defs.h
@@ -1,149 +1,35 @@
-#ifndef __DEFS_H__
-#define __DEFS_H__
+#ifndef __BOARD_H__
+#define __BOARD_H__
 
-#define _CRT_SECURE_NO_WARNINGS
+#ifndef _WIN32
+#define __DEV_BOARD__
+#endif
 
-#include "board.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
+#ifdef __DEV_BOARD__
+#include <Arduino.h>
+#define mySerial SerialUSB
+void printSerial(const char*);
+void loadSource(const char* source);
+#define __NEEDS_ALIGN__
+#define DICT_SZ (64*1024)
+#define STK_SZ 32
+#define TIB_SZ 0x0400
+#define WORDS_SZ 256
+#else
+#define DICT_SZ (64*1024)
+#define STK_SZ 32
+#define TIB_SZ 0x0400
+#define WORDS_SZ 256
+#define F(str) (char *)str
+#define PSTR(str) (char *)str
+#define strcmp_PF(str1, str2) strcmp(str1, str2)
+void loadSource(const char* source);
+typedef unsigned int uint;
+typedef unsigned long ulong;
+#define INPUT_PULLUP 0
+#define INPUT_PULLDOWN 0
+#define A_INPUT 0
+#define OUTPUT 0
+#endif
 
-typedef void (*FP)();
-typedef long  CELL;
-typedef unsigned long  UCELL;
-typedef unsigned short ADDR;
-typedef unsigned short WORD;
-typedef unsigned char  BYTE;
-
-#define CELL_SZ (4)
-#define WORD_SZ (2)
-#define ADDR_SZ (2)
-
-#define T dstk[sys->DSP]
-#define N dstk[sys->DSP-1]
-#define R rstk[sys->RSP]
-
-typedef struct {
-    ADDR XT;
-    BYTE dictionaryId;
-    BYTE flagsLen;
-    BYTE name[20];
-} DICT_T;
-
-typedef struct {
-    CELL autoRun;
-    BYTE currentDictId;
-    BYTE Reserved1;
-    BYTE Reserved2;
-    BYTE Reserved3;
-    CELL TIB;
-    CELL TOIN;
-    CELL HERE;
-    CELL LAST;
-    CELL BASE;
-    CELL STATE;
-    CELL DSTACK;
-    CELL RSTACK;
-    CELL DSP;
-    CELL RSP;
-} SYSVARS_T;
-
-//typedef struct {
-//    CELL addr;
-//    BYTE available;
-//    WORD sz;
-//} ALLOC_T;
-
-typedef struct {
-    char name[16];
-    byte opcode;
-    byte makeWord;
-} OPCODE_T;
-
-typedef struct {
-    CELL from, to, i;
-} LOOPINFO_T;
-
-#define ADDR_AUTORUN    (CELL_SZ*0)
-#define ADDR_RES_1      (CELL_SZ*1)
-#define ADDR_TIB        (CELL_SZ*2)
-#define ADDR_TOIN       (CELL_SZ*3)
-#define ADDR_HERE       (CELL_SZ*4)
-#define ADDR_LAST       (CELL_SZ*5)
-#define ADDR_BASE       (CELL_SZ*6)
-#define ADDR_STATE      (CELL_SZ*7)
-#define ADDR_DSTK       (CELL_SZ*8)
-#define ADDR_RSTK       (CELL_SZ*9)
-#define ADDR_DSP        (CELL_SZ*10)
-#define ADDR_RSP        (CELL_SZ*11)
-
-#define ADDR_HERE_BASE  (CELL_SZ*12)
-
-extern BYTE IR;
-extern CELL PC;
-extern int DSP, RSP;
-extern CELL HERE, LAST;
-extern CELL* dstk, * rstk;
-extern CELL BASE, STATE;
-extern FP prims[];
-extern BYTE dict[];
-extern SYSVARS_T* sys;
-extern CELL toIn;
-extern CELL allocAddrBase;
-extern CELL allocCurFree;
-extern int loopDepth;
-
-void dumpDict();
-void vmInit();
-void push(CELL);
-CELL pop();
-void rpush(CELL);
-CELL rpop();
-void run(CELL, CELL);
-CELL allocSpace(int);
-void allocFree(CELL);
-CELL stringToDict(char*, CELL);
-BYTE getOpcode(char*);
-CELL align2(CELL);
-CELL align4(CELL);
-BYTE nextChar();
-void is_hex(char*);
-void is_decimal(char*);
-void is_binary(char*);
-void CCOMMA(BYTE v);
-void WCOMMA(WORD v);
-void COMMA(CELL v);
-void ACOMMA(ADDR v);
-void parseLine(char*);
-void loadBaseSystem();
-BYTE getOpcode(char* w);
-void printString(const char* str);
-void printStringF(const char* fmt, ...);
-void loadUserWords();
-void ok();
-void autoRun();
-CELL cellAt(CELL);
-CELL wordAt(CELL);
-CELL addrAt(CELL);
-void fDUMPCODE();
-void wordStore(CELL addr, CELL val);
-void cellStore(CELL addr, CELL val);
-void addrStore(CELL addr, CELL val);
-void genOpcodeWords();
-CELL s4NumberAt(CELL);
-CELL s4addrAt(CELL);
-
-void fSTORE();           // OP_STORE
-void fEMIT();            // OP_EMIT
-void fTYPE();            // OP_TYPE
-void fDOTS();            // OP_DOTS
-void fPARSEWORD();       // OP_PARSEWORD
-void fPARSELINE();       // OP_PARSELINE
-void fCREATE();          // OP_CREATE
-void fFIND();            // OP_FIND
-void fNEXTWORD();        // OP_NEXTWORD
-void fISNUMBER();        // OP_ISNUMBER
-void fNUM2STR();         // OP_NUM2STR
-
-#endif // __DEFS_H__
+#endif
