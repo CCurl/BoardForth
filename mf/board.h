333a91a68132ecbfd1ba8a211a8c72ddb609a76a
Chris Curl <curlbud-github@yahoo.com>
2021-05-03 18:15:01 -0400

    Refactor mf (#10)
    * Add files via upload
    
    * progress
    
    * closer
    
    * more
    
    * it works!
    
    * update
    
    * add u., u/mod
    
    * some cleanup
    
    * 5/3 work (1)
    
    Co-authored-by: Chris Curl <curlbud-github.com>

--- mf/board.h
+++ mf/board.h
@@ -16,16 +16,21 @@
     #define TIB_SZ 0x0400
     #define ALLOC_SZ 16
 #else
-    #define DICT_SZ (64*1024)
-    #define STK_SZ 32
+    #include <windows.h>
+    #define DICT_SZ (512*1024)
+    #define STK_SZ 64
     #define TIB_SZ 0x0400
-    #define ALLOC_SZ 32
+    #define ALLOC_SZ 64
     #define F(str) (char *)str
     #define PSTR(str) (char *)str
     #define strcmp_PF(str1, str2) strcmp(str1, str2)
     void loadSource(const char *source);
     typedef unsigned int uint;
     typedef unsigned long ulong;
+    #define PIN_INPUT 0
+    #define PIN_INPUT_PULLUP 0
+    #define PIN_INPUT_PULLDOWN 0
+    #define PIN_OUTPUT 0
 #endif
 
 #endif
