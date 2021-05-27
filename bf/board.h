ccdcfcb8848af5ca289dab2925ccd60d97c31861
Chris Curl <curlbud-github@yahoo.com>
2021-05-27 19:07:12 -0400

    remove old bf (#14)
    * Remove old code
    
    * Make it build
    
    * update make.bat
    
    * update test program for LittleFS
    
    * for littlefs testing
    
    Co-authored-by: Chris Curl <curlbud-github.com>

--- bf/board.h
+++ bf/board.h
@@ -2,27 +2,43 @@
 #define __BOARD_H__
 
 #ifndef _WIN32
-    #define __DEV_BOARD__
+	#define __DEV_BOARD__
+	#include <Arduino.h>
+	#define mySerial SerialUSB
+	// #define mySerial Serial
+	// #define __ESP32__
+    #include <LittleFS.h>
+    #define __LITTLEFS__
+	#define __JOYSTICK__
+	void printSerial(const char*);
+	void loadSource(const char* source);
+	#define __NEEDS_ALIGN__
+	#define DICT_SZ (80*1024)
+	#define VARS_SZ (32*1024)
+	#define STK_SZ 32
+	#define TIB_SZ 0x0400
+	#define MAX_FILE_SZ 10*1024
+	#define PIN_INPUT INPUT
+	#define PIN_INPUT_PULLUP INPUT_PULLUP
+	#define PIN_OUTPUT OUTPUT
+#else
+	#include <windows.h>
+	#define DICT_SZ (512*1024)
+	#define VARS_SZ (64*1024)
+	#define STK_SZ 64
+	#define TIB_SZ 0x0400
+	#define MAX_FILE_SZ 10*1024
+	#define __FILES__
+	#define __COM_PORT__
+	void loadSource(const char* source);
+	typedef unsigned int uint;
+	typedef unsigned long ulong;
+	#define PIN_INPUT 1
+	#define PIN_INPUT_PULLUP 2
+	#define PIN_OUTPUT 3
 #endif
 
-#ifdef __DEV_BOARD__
-    #include <Arduino.h>
-    #define mySerial Serial
-    void printSerial(const char *);
-    void loadSource(const char *source);
-    #define __NEEDS_ALIGN__
-    #define DICT_SZ (64*1024)
-    #define STK_SZ 32
-    #define TIB_SZ 0x0400
-    #define WORDS_SZ 0x200
-#else
-    #include <windows.h>
-    #define DICT_SZ (64*1024)
-    #define STK_SZ 32
-    #define TIB_SZ 0x0400
-    #define WORDS_SZ 0x200
-    #define F(str) (char *)str
-    #define PSTR(str) (char *)str
-#endif
+// Optional wordsets
+#define __ARDUINO__
 
 #endif
--- bf/make.bat
+++ bf/make.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if exist bf.exe ( del bf.exe )
-gcc -Ofast -o bf.exe forth.cpp main.cpp
+gcc -Ofast -o bf.exe forth.cpp
 if xx%1xx == xxxx goto :done
 if exist bf.exe ( bf.exe )
 
